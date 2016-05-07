990d2b35313c2c2a0a5a7d43d89fe9c1a39d8377
Mike <zhihaochi@gmail.com>
2016-05-07 18:09:02 -0400

    Now only extracting the largest classes.jar files in the AAR
    Some library AAR's include smaller jar files with auxiliary classes. I
    have found that these are not all that important for identifying a
    library in an APK.  The largest classes.jar file often contains the meat
    of the library code, and is what we are interested in.

--- src/com/zchi88/android/libdiff/utilities/AarToJar.java
+++ src/com/zchi88/android/libdiff/utilities/AarToJar.java
@@ -13,14 +13,14 @@ import java.util.jar.JarFile;
 public class AarToJar {
 	/**
 	 * Searches a library for any AAR files. If found, the JAR file will be
-	 * extracted and the AAR file will be removed. This is necessary since 
+	 * extracted and the AAR file will be removed. This is necessary since
 	 * current Java decompilers are designed to decompile JAR files.
 	 * 
 	 * @throws IOException
 	 */
 	public static void convertAarToJar(Path library) throws IOException {
 		File[] libraryVersions = library.toFile().listFiles();
-	
+
 		if (libraryVersions.length > 0) {
 			for (File libFile : libraryVersions) {
 				String nameOfLib = libFile.toString();
@@ -30,58 +30,63 @@ public class AarToJar {
 			}
 		}
 	}
-	
-	
-	/** 
+
+	/**
 	 * Extracts the classes.jar file found in the AAR, and then deletes the AAR.
-	 * Note that although this uses the java JarFile class, it works for unzipping AAR files as well.
+	 * Note that although this uses the java JarFile class, it works for
+	 * unzipping AAR files as well.
 	 * 
-	 * @param aarPath - the String representation of the file path to the AAR
+	 * @param aarPath
+	 *            - the String representation of the file path to the AAR
 	 * @throws IOException
 	 */
-	public static void extractJar(String aarPath) throws IOException{
-		int jarCount = 0;
+	public static void extractJar(String aarPath) throws IOException {
 		File pathToAar = new File(aarPath);
 		String nameOfAar = pathToAar.getName();
 		JarFile aar = new JarFile(aarPath);
 		Enumeration<JarEntry> filesInAar = aar.entries();
-		
+
 		System.out.format("AAR file found. Attempting to extract classes.jar from %s...\n", pathToAar);
-		
+
 		while (filesInAar.hasMoreElements()) {
 			java.util.jar.JarEntry file = filesInAar.nextElement();
 
-			
-			// TODO: make sure that an AAR file will not contain more than one classes.jar file. 
+			long sizeTracker = 0;
+
 			if (file.getName().endsWith(".jar")) {
-				jarCount++;
+				// Get the largest jar, which should contain most if not all of
+				// the library's code
+				long fileSize = file.getSize();
+
 				String nameOfJar;
-				if (jarCount < 2) {	// Name the JAR after the name of the AAR file if there is only one classes.jar file 
-					nameOfJar = nameOfAar.replace(".aar", ".jar");
-				} else { // In case there are multiple classes.jar files, differentiate their names by appending a number to them.
-					nameOfJar = nameOfAar.replace(".aar", "(" + jarCount + ")" + ".jar");
-				}
+				nameOfJar = nameOfAar.replace(".aar", ".jar");
+
 				File newFile = new File(pathToAar.getParent() + java.io.File.separator + nameOfJar);
+
+				if (fileSize > sizeTracker) {
+					sizeTracker = fileSize;
+					if (newFile.exists()) {
+						newFile.delete();
+					}
+				}
+
 				InputStream is = aar.getInputStream(file);
 				FileOutputStream os = new FileOutputStream(newFile);
-				while (is.available() > 0) {  // Write contents of InputStream to FileOutputStream
+				while (is.available() > 0) { // Write contents of InputStream to
+												// FileOutputStream
 					os.write(is.read());
 				}
 				os.close();
 				is.close();
 			}
 		}
-		
+
 		aar.close();
-		
-		if (jarCount > 0) {
-			System.out.println("JAR extraction completed!");
-			deleteAar(aarPath);
-		} else {
-			System.out.println("No JAR file(s) found!");
-		}
+
+		System.out.println("JAR extraction completed!");
+		deleteAar(aarPath);
 	}
-	
+
 	// Deletes the AAR file
 	private static void deleteAar(String aarPath) throws IOException {
 		System.out.println("Deleting the AAR file...");
